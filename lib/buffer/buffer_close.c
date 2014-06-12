5f93baff178db061632400316bc9f8c9930cdccf
Roman Senn <roman.l.senn@gmail.com>
2014-06-12 08:10:14 +0200

    proper munmap

--- lib/buffer/buffer_close.c
+++ lib/buffer/buffer_close.c
@@ -1,22 +1,13 @@
 #include <buffer.h>
-#include <shell.h>
+#include <stdlib.h>
 #include <unistd.h>
-#ifdef __MINGW32__
-#include <windows.h>
-#else
-#include <sys/mman.h>
-#endif
+#include "mmap.h"
 
 void buffer_close(buffer* b) {
   if (b->fd != -1) close(b->fd);
   switch (b->todo) {
-  case FREE: shell_free(b->x); break;
-  case MUNMAP:
-#ifdef __MINGW32__
-    UnmapViewOfFile(b->x);
-#else
-    munmap(b->x,b->a); break;
-#endif
+  case FREE: free(b->x); break;
+  case MUNMAP: mmap_unmap(b->x,b->a); break;
   default: ;
   }
 }
