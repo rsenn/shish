b6d7ace777937918456feaf7dee0d543fc5c33da
Roman Senn <roman.l.senn@gmail.com>
2014-06-12 07:26:52 +0200

    ...

--- lib/buffer/buffer_fromsa.c
+++ lib/buffer/buffer_fromsa.c
@@ -1,10 +1,8 @@
 #include "stralloc.h"
 #include "buffer.h"
 
-static ssize_t dummyreadwrite(int fd,char* buf,size_t len) {
-  (void)fd;
-  (void)buf;
-  (void)len;
+ssize_t buffer_dummyread(int fd,char* buf,unsigned long int len) 
+{
   return 0;
 }
 
@@ -13,6 +11,6 @@ void buffer_fromsa(buffer* b,stralloc* sa) {
   b->p=0;
   b->n=sa->len;
   b->a=sa->a;
-  b->fd=0;
-  b->op=dummyreadwrite;
+  b->fd=-1;
+  b->op=buffer_dummyread;
 }
