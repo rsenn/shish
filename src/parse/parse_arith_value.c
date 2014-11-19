ebfec3ae6582e90f70479fd69de5ad95f989666d
Roman Senn <roman.l.senn@gmail.com>
2014-11-19 04:05:27 +0100

    ...

---
+++ src/parse/parse_arith_value.c
@@ -0,0 +1,38 @@
+#include "tree.h"
+#include "parse.h"
+#include "source.h"
+#include "fmt.h"
+#include "scan.h"
+
+/* parse arithmetic value
+ * ----------------------------------------------------------------------- */
+union node *parse_arith_value(struct parser *p)
+{
+	union node* node = 0;
+	char x[FMT_LONG+1];
+	long long num;
+
+	if(source_peekn(x,FMT_LONG) <= 0)
+		return 0;
+
+	if(scan_longlong(x, &num) > 0) {
+		node = tree_newnode(N_ARITH_NUM);
+		node->narithnum.num = num;
+	} else {
+		char c;
+		stralloc w;
+		stralloc_init(&w);
+
+		while(source_peek(&c) > 0 && !parse_isspace(c)) {
+			stralloc_catc(&w, c);
+			source_skip();
+		}
+		stralloc_catc(&w, '\0');
+
+		node = tree_newnode(N_ARITH_VAR);
+		node->narithvar.var = w.s;
+	}
+
+  return node;
+}
+
--- src/parse/parse_compound_list.c
+++ src/parse/parse_compound_list.c
@@ -11,7 +11,7 @@ union node *parse_compound_list(struct parser *p) {
   union node *list;
   union node **nptr;
 
-  tree_init(list, nptr);
+	  tree_init(list, nptr);
 
   /* skip arbitrary newlines */
   while(parse_gettok(p, P_DEFAULT) & T_NL);
