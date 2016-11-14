52da49ecb5685f8b45df979d13621c7c27b49018
Roman Senn <roman.l.senn@gmail.com>
2016-11-14 21:58:30 +0100

    ...

--- lib/shell/shell_reallocdebug.c
+++ lib/shell/shell_reallocdebug.c
@@ -4,24 +4,13 @@
 extern void *debug_realloc(const char *file, unsigned int line, void *ptr, unsigned long size);
 
 void *shell_reallocdebug(const char *file, unsigned int line, void *ptr, unsigned long size)
-#else
-void *shell_realloc(void *ptr, unsigned long size)
-#endif /* DEBUG */
 {
   void *newptr;
 
   if(ptr == NULL)
-#ifdef DEBUG
     return shell_allocdebug(file, line, size);
-#else
-    return shell_alloc(size);
-#endif /* DEBUG */
 
-#ifdef DEBUG
   newptr = debug_realloc(file, line, ptr, size);
-#else
-  newptr = realloc(ptr, size);
-#endif /* DEBUG */
 
   /* exit if failed */
   if(newptr == NULL) {
@@ -32,3 +21,4 @@ void *shell_realloc(void *ptr, unsigned long size)
   /* return pointer otherwise */
   return newptr;
 }
+#endif /* defined(DEBUG) */
