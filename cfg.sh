8705a632b3570f0e91e7daacbba81d51127bffa6
Roman Senn <you@example.com>
2018-02-01 01:24:54 +0100

    ...

--- cfg.sh
+++ cfg.sh
@@ -1,25 +1,57 @@
 cfg() { 
+  [ "$DEBUG" = 1 ] && TYPE=Debug
+  [ "$VERBOSE" = 1 ] && VERBOSE_MAKEFILE=TRUE
+
   case $(uname -o) in
-    MSys|MSYS|Msys) SYSTEM="MSYS" ;;
-    *) SYSTEM="Unix" ;;
+      MSys|MSYS|Msys) SYSTEM="MSYS" ;;
+      *) SYSTEM="Unix" ;;
   esac
 
-  for builddir in build/cmake{-debug,,-release,-minsizerel}
-  do
-    case "$builddir" in
-            *debug*) TYPE=Debug ;;
-            *reease*) TYPE=Release ;;
-            *minsiz*) TYPE=MinSizeRel ;;
-            *) TYPE=RelWithDebInfo ;;
-    esac
- (mkdir -p $builddir
+  [ -n "$TYPE" ] && build=cmake-$(echo "$TYPE" | tr "[[:upper:]]" "[[:lower:]]")
+
+  libdir=`get-libdir`
+  builddir=build/${build:-cmake}
+  : ${prefix=/usr}
+
+ (mkdir -p $builddir;
   set -x
-  cd $builddir
-  cmake -Wno-dev -DCMAKE_INSTALL_PREFIX=$(get-prefix) \
+  cd $builddir;
+  set -x
+  cmake -Wno-dev -DCMAKE_INSTALL_PREFIX="$prefix" \
     -G "${SYSTEM:-MSYS} Makefiles" \
-    ${VERBOSE+-DCMAKE_VERBOSE_MAKEFILE=TRUE} \
+    -DCMAKE_C_COMPILER="${CC-gcc}" \
+    ${VERBOSE_MAKEFILE:+-DCMAKE_VERBOSE_MAKEFILE="$VERBOSE_MAKEFILE"} \
     -DCMAKE_BUILD_TYPE="${TYPE:-RelWithDebInfo}" \
-    "$@" \
-  ../..)
-  done
+  "$@" \
+   ../..)
+}
+
+get-libdir() {
+  ${CC:-gcc} -print-search-dirs |sed -n '/libraries/ { s,^libraries: =,, ; s,:,\n,g; p }'|grep -v -E '(/\.\.|/gcc|/[0-9]/|^/lib)' |sed -n '1 { s,/$,,  ; p }'
+}
+
+dietcfg() {
+  unset builddir
+  build=$(gcc -dumpmachine)
+  host=${build//gnu/dietlibc}
+  case "$CC $*" in
+    *-m32*) host=i686-${host#*-} ;;
+    *i[3-6]86*) host=i686-${host#*-} ;;
+  esac
+  case "$host" in
+  i?86*) a=i686 m="-m32" ;;
+  *) a=x86_64 m="-m64" ;;
+  esac
+  DIET=/opt/diet/bin-$a/diet
+  prefix=${DIET%%/bin*}
+  bindir=${prefix}/bin-${host%%-*}
+  libdir=${prefix}/lib-${host%%-*}
+  
+  builddir="build/cmake-diet" 
+  
+  CC="i686-linux-dietlibc-gcc" \
+  CXX="i686-linux-dietlibc-g++" \
+  build="$host"  \
+  cfg \
+  "$@"
 }
