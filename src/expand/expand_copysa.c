5174baa2ac0b85fbc8b0b6cc377101897d8c4765
Roman Senn <roman.l.senn@gmail.com>
2020-01-08 06:06:23 +0100

    ...

---
+++ src/expand/expand_copysa.c
@@ -0,0 +1,18 @@
+#include "byte.h"
+#include "expand.h"
+#include "tree.h"
+
+/* expand one N_ARG node to a stralloc (stralloc is overwritten!!!)
+ * ----------------------------------------------------------------------- */
+void
+expand_copysa(union node* node, stralloc* sa) {
+  union node tmpnode;
+
+  struct expand x = EXPAND_INIT(&tmpnode, 0, X_NOSPLIT);
+  x.ptr = &x.root;
+
+  stralloc_init(&tmpnode.narg.stra);
+  expand_arg(&x, &node->narg);
+  byte_copy(sa, sizeof(stralloc), &tmpnode.narg.stra);
+  stralloc_nul(sa);
+}
