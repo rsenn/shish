aec7219e67a4f942cb9080cfdad14955b47dd9b7
Roman <roman.l.senn@gmail.com>
2020-01-29 09:11:58 +0100

    ...

--- src/exec/exec_search.c
+++ src/exec/exec_search.c
@@ -5,13 +5,16 @@
  * ----------------------------------------------------------------------- */
 struct exechash*
 exec_search(char* name, uint32* hashptr) {
-  uint32 hash = *hashptr = exec_hashstr(name);
+  uint32 hash = exec_hashstr(name);
   struct exechash* entry;
 
   for(entry = exec_hashtbl[hash & EXEC_HASHMASK]; entry; entry = entry->next) {
-    if(entry->hash == hash)
-      if(!str_diff(entry->name, name))
+    if(entry->hash == hash) {
+      if(!str_diff(entry->name, name)) {
+        if(hashptr) *hashptr = hash;
         return entry;
+      }
+    }
   }
 
   return NULL;
