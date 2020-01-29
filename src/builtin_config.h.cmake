958549a26cb5c53bb23248782a77828c8ee55620
Roman <roman.l.senn@gmail.com>
2020-01-29 07:14:48 +0100

    ...

---
+++ src/builtin_config.h.cmake
@@ -0,0 +1,19 @@
+
+#cmakedefine BUILTIN_BREAK
+#cmakedefine BUILTIN_CD
+#cmakedefine BUILTIN_DUMP
+#cmakedefine BUILTIN_ECHO
+#cmakedefine BUILTIN_EVAL
+#cmakedefine BUILTIN_EXEC
+#cmakedefine BUILTIN_EXIT
+#cmakedefine BUILTIN_EXPORT
+#cmakedefine BUILTIN_FALSE
+#cmakedefine BUILTIN_FDTABLE
+#cmakedefine BUILTIN_HASH
+#cmakedefine BUILTIN_HISTORY
+#cmakedefine BUILTIN_PWD
+#cmakedefine BUILTIN_SET
+#cmakedefine BUILTIN_SHIFT
+#cmakedefine BUILTIN_SOURCE
+#cmakedefine BUILTIN_TRUE
+#cmakedefine BUILTIN_UNSET
