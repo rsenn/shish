13856676438258cf7c85d4eddeb16296c3e0bdfc
Roman <roman.l.senn@gmail.com>
2020-02-04 02:12:42 +0100

    ...

--- src/builtin/builtin_dirname.c
+++ src/builtin/builtin_dirname.c
@@ -4,12 +4,25 @@
 /* ----------------------------------------------------------------------- */
 int
 builtin_dirname(int argc, char** argv) {
+  char* path;
+
   if(argc < 2) {
     builtin_errmsg(argv, "too few arguments", NULL);
     return 1;
   }
 
-  buffer_puts(fd_out->w, dirname(argv[1]));
+  path = argv[1];
+  if(path) {
+    int i;
+    for(i = str_len(path) - 1; i >= 0; i--) {
+      if(path[i] == '/') {
+        path[i] = '\0';
+        break;
+      }
+    }
+  }
+
+  buffer_puts(fd_out->w, path);
   buffer_putnlflush(fd_out->w);
   return 0;
 }
