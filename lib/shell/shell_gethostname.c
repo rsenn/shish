d59c26929c9e64098b3dc885fd9acf4a01730595
Roman <roman.l.senn@gmail.com>
2020-02-04 02:27:31 +0100

    ...

--- lib/shell/shell_gethostname.c
+++ lib/shell/shell_gethostname.c
@@ -4,8 +4,17 @@
 #include "../windoze.h"
 
 #if WINDOWS_NATIVE
-#include <winsock2.h>
+#include <windows.h>
+#define GHN_BUFSIZE MAX_COMPUTERNAME_LENGTH
+#define gethostname(x,n) get_host_name(x,n)
+static int get_host_name(char* x, size_t n) {
+  DWORD len = n; 
+  if(GetComputerNameA(x,&len))
+    return 0;
+  return -1;
+}
 #else
+#define GHN_BUFSIZE 64
 #include <unistd.h>
 #endif
 
@@ -15,7 +24,7 @@ shell_gethostname(stralloc* sa) {
 
   do {
     n += 64;
-    stralloc_ready(sa, n);
+    stralloc_ready(sa, n+1);
   } while(gethostname(sa->s, sa->a));
 
   if(sa->s) {
