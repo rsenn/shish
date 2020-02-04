1a134a27a964fc3f8718f8e9893de1986e7bd6a5
Roman <roman.l.senn@gmail.com>
2020-02-04 08:54:35 +0100

    ...

--- gen-mk.cmd
+++ gen-mk.cmd
@@ -4,7 +4,7 @@ for %%b in (Debug RelWithDebInfo MinSizeRel Release) do (
   for %%t in (lcc32 lcc64 tcc32 tcc64 bcc55 bcc101) do (
     mkdir build/%%t/%%b
 
-	genmakefile -s windows -m ninja -t %%t --%%b lib src -o build/%%t/%%b/build.ninja
-	genmakefile -s windows -m make -t %%t --%%b lib src -o build/%%t/%%b/Makefile
+    genmakefile --create-bins --no-create-libs -m ninja -t %%t --%%b lib src -o build/%%t/%%b/build.ninja
+    genmakefile --create-bins --no-create-libs -m make -t %%t --%%b lib src -o build/%%t/%%b/Makefile
   )
 )
