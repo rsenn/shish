9f112ca07482a1c15b5c1454e8ef9458f7f5dc41
Roman <roman.l.senn@gmail.com>
2020-02-04 05:49:00 +0100

    ...

--- lib/wait.h
+++ lib/wait.h
@@ -1,13 +1,8 @@
-#ifndef WAIT_H
-#define WAIT_H
+#ifndef _WAIT_H
+#define _WAIT_H
 
 #include "uint64.h"
 #include "windoze.h"
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-
 
 #if WINDOWS_NATIVE
 #define WAIT_IF_SIGNALED(status) ((status)&0)
@@ -24,8 +19,4 @@ int wait_pid_nohang(int pid, int* wstat);
 int waitpid_nointr(int pid, int* wstat, int flags);
 int wait_pids_nohang(int const* pids, unsigned int len, int* wstat);
 
-#ifdef __cplusplus
-}
-#endif
-
-#endif // WAIT_H
+#endif // _WAIT_H
