6634d67299a2a1f61985da3de3cb88e71c5d455a
Roman <roman.l.senn@gmail.com>
2020-02-04 03:44:13 +0100

    ...

--- src/vartab/vartab_root.c
+++ src/vartab/vartab_root.c
@@ -1,5 +1,10 @@
 #include "vartab.h"
 
-struct vartab vartab_root = {.pos = NULL, .parent = NULL, .level = 0};
+struct vartab vartab_root = {{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                              0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                              0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                             0,
+                             0,
+                             0};
 struct vartab* varstack = &vartab_root;
 struct var* var_list = NULL;
