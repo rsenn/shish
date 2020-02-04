ea3fad1da7b44631210994c087f2d0efb5e5143d
Roman <roman.l.senn@gmail.com>
2020-02-04 09:04:36 +0100

    ...

--- gen-mk.cmd
+++ gen-mk.cmd
@@ -2,12 +2,23 @@
 
 for %%t in (lcc32 lcc64 tcc32 tcc64 bcc55 bcc101) do (
   for %%b in (Debug RelWithDebInfo MinSizeRel Release) do (
-    if not exist build\%%t\%%b mkdir build\%%t\%%b
-    del /f /q /s build\%%t\%%b\*
-
-    echo on
-    genmakefile --create-bins --no-create-libs -m ninja -t %%t --%%b lib src -o build\%%t\%%b\build.ninja
-    genmakefile --create-bins --no-create-libs -m make -t %%t --%%b lib src -o build\%%t\%%b\Makefile
-    @echo off
+  
+    call :gen %%t %%b
+  
   )
 )
+goto :end
+
+:gen
+set output_dir=build\%1\%2
+if not exist %output_dir% mkdir %output_dir%
+del /f /q /s %output_dir%\*
+
+echo on
+genmakefile --create-bins --no-create-libs -m ninja -t %1 --%2 lib src -o %output_dir%\build.ninja
+genmakefile --create-bins --no-create-libs -m make -t %1 --%2 lib src -o %output_dir%\Makefile
+
+@echo off
+
+
+:end
