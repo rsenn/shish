1523428d1dae2c70b544f8be045fda3bb05d2adb
Roman <roman.l.senn@gmail.com>
2020-02-04 08:26:50 +0100

    ...

--- gen-mk.sh
+++ gen-mk.sh
@@ -5,7 +5,7 @@ for build_type in Debug RelWithDebInfo MinSizeRel Release; do
 	output_dir=build/$compiler/$build_type
 	mkdir -p $output_dir
 
-	genmakefile -s windows -m ninja -t $compiler --$build_type lib src -o $output_dir/build.ninja
-	genmakefile -s windows -m nmake -t $compiler --$build_type lib src -o $output_dir/Makefile
+	genmakefile --create-bins --no-create-libs -s windows -m ninja -t $compiler --$build_type lib src -o $output_dir/build.ninja
+	genmakefile --create-bins --no-create-libs -s windows -m nmake -t $compiler --$build_type lib src -o $output_dir/Makefile
   done
 done
