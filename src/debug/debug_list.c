72512e5f8534b0653d545cb772d0a6e2f78c6cd1
Roman <roman.l.senn@gmail.com>
2020-02-15 14:10:09 +0100

    ...

--- src/debug/debug_list.c
+++ src/debug/debug_list.c
@@ -1,9 +1,7 @@
 #include "../debug.h"
-
 #ifdef DEBUG_OUTPUT
 #include "../fd.h"
 #include "../tree.h"
-
 /* debug a list/tree
  * ----------------------------------------------------------------------- */
 void
@@ -11,23 +9,19 @@ debug_list(union node* n, int depth) {
   unsigned long i = 0;
   union node* node;
   int id = n->id;
-
   if(id == N_ARG) {
     if(i == 0)
       buffer_puts(fd_err->w, "\n ");
-      buffer_putnspace(fd_err->w, depth > 0 ? (depth -1) * 2 : 1);
   }
+      buffer_putnspace(fd_err->w, depth > 0 ? (depth - 1) * 2 : 1);
+
   for(node = n; node; i++, node = node->list.next) {
-    /*
-        if(depth > 0 )
-      buffer_putnspace(fd_err->w, (depth ) * 2);*/
     if(id == N_ARG) {
-      /*   if(i == 0)
-           buffer_putnspace(fd_err->w, depth > 0 ? depth * 2 : id == N_ARG && depth != -2 ? 4 : 0);
-   */
       buffer_putulong(fd_err->w, i);
       buffer_puts(fd_err->w, ": ");
     }
+    if(id == N_ARG)
+      buffer_puts(fd_err->w, COLOR_CYAN "[" COLOR_NONE);
     debug_node(node, id >= N_ARGSTR ? -2 : id == N_ARG ? -1 : depth);
     if(node->list.next) {
       if(depth > 0 && id != N_ARG) {
@@ -38,9 +32,8 @@ debug_list(union node* n, int depth) {
       } else if(depth == -2) {
         buffer_puts(fd_err->w, ", ");
       }
-
       if(id == N_ARG) {
-        buffer_putnspace(fd_err->w, depth );
+        buffer_putnspace(fd_err->w, depth);
       }
     }
   }
