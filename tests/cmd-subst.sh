b470a58fdb825f1cb17ad0377751631b3cd45215
Roman Senn <roman.l.senn@gmail.com>
2021-03-25 05:27:26 +0100

    ...

--- tests/cmd-subst.sh
+++ tests/cmd-subst.sh
@@ -3,6 +3,13 @@
 
 OUTPUT=$(echo "This is output from an 'echo' command")
 echo "OUTPUT='$OUTPUT'" 1>&2
-FORMATTED=`printf "0x%08X" $((0xdeadbeef))`
 
+FORMATTED=`printf "0x%08X" $((0xdeadbeef))`
 echo "FORMATTED='$FORMATTED'" 1>&2
+
+X=1337
+HERE=$(cat <<EOF
+Here doc content $X
+EOF
+)
+echo "HERE='$HERE'" 1>&2
