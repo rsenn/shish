27998d115e11adcb3ba660062e911380856982cb
Roman Senn <roman.l.senn@gmail.com>
2021-03-30 10:49:59 +0200

    ...

--- src/expand/expand_arith_assign.c
+++ src/expand/expand_arith_assign.c
@@ -3,46 +3,43 @@
 #include "../var.h"
 #include "../../lib/uint64.h"
 #include "../../lib/scan.h"
+#include <assert.h>
 
 /* expand a assignment expression
  * ----------------------------------------------------------------------- */
 int
 expand_arith_assign(struct narithbinary* assign, int64* r) {
 
-  if(assign->left->id == N_ARGPARAM) {
-    int64 left = 0, right = 0;
-    size_t offset;
-    const char* s;
-    struct var* v;
-    struct nargparam* param = &assign->left->nargparam;
+  int64 left = 0, right = 0;
+  size_t offset;
+  char buf[FMT_LONG * 2];
 
-    if((s = var_vdefault(param->name,"0", 0)) == 0 || !scan_longlong(s, &left))
-      left = 0;
+  const char *s, *name = assign->left->nargparam.name;
+  assert(assign->left->id == N_ARGPARAM);
 
-    if(expand_arith_expr(assign->right, &right))
-      right = 0;
+  if((s = var_vdefault(name, "0", 0)) == 0 || !scan_longlong(s, &left))
+    left = 0;
 
-    switch(assign->id) {
-      case A_ASSIGN: left = right; break;
-      case A_ASSIGNADD: left += right; break;
-      case A_ASSIGNSUB: left -= right; break;
-      case A_ASSIGNMUL: left *= right; break;
-      case A_ASSIGNDIV: left /= right; break;
-      case A_ASSIGNMOD: left %= right; break;
-      case A_ASSIGNLSHIFT: left <<= right; break;
-      case A_ASSIGNRSHIFT: left >>= right; break;
-      case A_ASSIGNBAND: left &= right; break;
-      case A_ASSIGNBXOR: left ^= right; break;
-      case A_ASSIGNBOR: left |= right; break;
-      default: return 1;
-    }
+  if(expand_arith_expr(assign->right, &right))
+    right = 0;
 
-    {
-      char buf[FMT_LONG * 2];
-      var_setv(param->name, buf, fmt_longlong(buf, left), V_DEFAULT);
-    }
-
-    *r = left;
+  switch(assign->id) {
+    case A_ASSIGN: left = right; break;
+    case A_ASSIGNADD: left += right; break;
+    case A_ASSIGNSUB: left -= right; break;
+    case A_ASSIGNMUL: left *= right; break;
+    case A_ASSIGNDIV: left /= right; break;
+    case A_ASSIGNMOD: left %= right; break;
+    case A_ASSIGNLSHIFT: left <<= right; break;
+    case A_ASSIGNRSHIFT: left >>= right; break;
+    case A_ASSIGNBAND: left &= right; break;
+    case A_ASSIGNBXOR: left ^= right; break;
+    case A_ASSIGNBOR: left |= right; break;
+    default: return 1;
   }
+
+  var_setv(name, buf, fmt_longlong(buf, left), V_DEFAULT);
+
+  *r = left;
   return 0;
 }
---
+++ src/expand/expand_arith_value.c
@@ -0,0 +1,42 @@
+#include "../expand.h"
+#include "../tree.h"
+#include "../../lib/uint64.h"
+#include "../var.h"
+#include <math.h>
+
+/* expand a unary expression
+ * ----------------------------------------------------------------------- */
+int
+expand_arith_value(union node* node, int64* r) {
+  int64 value;
+  struct nargparam* param = 0;
+  int pre_post = expr->id == A_PREINCREMENT || expr->id == A_PREDECREMENT || expr->id == A_POSTINCREMENT || expr->id == A_POSTDECREMENT;
+
+  if(expand_arith_expr(expr->node, &value))
+    return 1;
+
+  if(pre_post) {
+    param = &expr->node->nargparam;
+
+    var_search(param->name, NULL);
+  }
+
+  switch(expr->id) {
+    case A_UNARYMINUS: *r = 0 - value; break;
+    case A_UNARYPLUS: *r = value; break;
+    case A_PREINCREMENT: *r = ++value; break;
+    case A_POSTINCREMENT: *r = value++; break;
+    case A_PREDECREMENT: *r = --value; break;
+    case A_POSTDECREMENT: *r = value--; break;
+    case A_NOT: *r = !value; break;
+    case A_BNOT: *r = ~value; break;
+    default: break;
+  }
+
+  if(pre_post) {
+    if(param && param->name)
+      var_setvint(param->name, value, 0);
+  }
+
+  return 0;
+}
