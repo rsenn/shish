f418cedc44b71e161860b136476261b5d157228f
Roman Senn <roman.l.senn@gmail.com>
2021-12-13 11:40:43 +0100

    ...

---
+++ config.h
@@ -0,0 +1,88 @@
+/* config.h.cmake  Substituted by CMake  */
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "shish"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "shish-0.8"
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "0.8"
+
+/* Define this if you have alloca.h */
+#define HAVE_ALLOCA_H 1
+
+/* Define to header file needed for alloca() */
+#define ALLOCA_HEADER "alloca.h"
+
+/* Define this if your compiler supports alloca() */
+#define HAVE_ALLOCA 1
+
+/* Define this if you have sys/types.h */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define this if you have glob.h */
+#define HAVE_GLOB_H 1
+
+/* Define this if you have sys/wait.h */
+#define HAVE_SYS_WAIT_H 1
+
+/* Define this if you have signal.h */
+#define HAVE_SIGNAL_H 1
+
+/* Define this if you have stdint.h */
+#define HAVE_STDINT_H 1
+
+/* Define this if you have inttypes.h */
+#define HAVE_INTTYPES_H 1
+
+/* Define this if you have sys/stat.h */
+#define HAVE_SYS_STAT_H 1
+
+/* Define this if you have unistd.h */
+#define HAVE_UNISTD_H 1
+
+/* Define this if you have the lstat() function */
+#define HAVE_LSTAT 1
+
+/* Define this if you have the fcntl() function */
+#define HAVE_FCNTL 1
+
+/* Define this if you have the readlink() function */
+#define HAVE_READLINK 1
+
+/* Define this if you have the sethostname() function */
+#define HAVE_SETHOSTNAME 1
+
+/* Define this if you have the sigaction() function */
+#define HAVE_SIGACTION 1
+
+/* Define this if you have the sigprocmask() function */
+#define HAVE_SIGPROCMASK 1
+
+/* Define this if you have the sigsetmask() function */
+/* #undef HAVE_SIGSETMASK */
+
+/* Define this if you have the glob() function */
+#define HAVE_SETPGID 1
+
+/* Define this if you have the setpgid() function */
+#define HAVE_GLOB 1
+
+/* Define this if your libc exports sys_siglist[] */
+#define HAVE_SYS_SIGLIST 1
+
+/* Define this if you have a prototype declaratin for sys_siglist[]  */
+#define HAVE_SYS_SIGLIST_DECLARATION 1
+
+/* Define this if you have the 'sigset_t' type */
+#define HAVE_SIGSET_T 1
+
+/* Define this if you have the 'pid_t' type */
+#define HAVE_PID_T 1
+
+/* Define this if you have the 'uid_t' type */
+#define HAVE_UID_T 1
+
+/* Define this if you have fcntl.h */
+#define HAVE_FCNTL_H 1
